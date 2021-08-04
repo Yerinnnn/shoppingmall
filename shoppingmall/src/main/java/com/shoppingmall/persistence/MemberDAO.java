afd19c5eb83fca8f55e72af76238155a37ee1374
shinyerin <37470054+Yerinnnn@users.noreply.github.com>
2021-08-05 00:16:49 +0900

    Delete shoppingmall directory

--- shoppingmall/src/main/java/com/shoppingmall/persistence/MemberDAO.java
+++
@@ -1,8 +0,0 @@
-package com.shoppingmall.persistence;
-
-import com.shoppingmall.domain.MemberVO;
-
-public interface MemberDAO {
-
-	public void signup(MemberVO vo) throws Exception;
-}
--- shoppingmall/src/main/java/com/shoppingmall/service/MemberService.java
+++
@@ -1,8 +0,0 @@
-package com.shoppingmall.service;
-
-import com.shoppingmall.domain.MemberVO;
-
-public interface MemberService {
-	// 회원가입
-	public void signup(MemberVO vo) throws Exception;
-}
